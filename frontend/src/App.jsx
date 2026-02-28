60fcfe1daaa19f50bf6319ca4541904c5897c2de
J0Hn-git <johnjoji2222@gmail.com>
2026-02-28 13:36:46 +0530

    Home Page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Navbar from './components/Navbar/Navbar'
+import {Route, Routes} from 'react-router-dom'
 
 const App = () => {
   return(
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
